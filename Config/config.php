87125f9ae63e9d19eea90de526095d1ec97571b7
DaviMenezes <davimenezes.dev@gmail.com>
2022-05-21 01:31:01 -0300

    text-colors and text-sizes configs

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,29 @@
 <?php
 
 return [
-    'name' => 'DvUi'
+    'name' => 'DvUi',
+    'text-colors' => [
+        'text-white',
+        'text-gray',
+        'text-purple',
+        'text-yellow',
+        'text-green',
+        'text-pink',
+        'text-red',
+        'text-slate'
+    ],
+    'text-sizes' => [
+        'text-xs',
+        'text-sm',
+        'text-base',
+        'text-lg',
+        'text-2',
+        'text-3',
+        'text-4',
+        'text-5',
+        'text-6',
+        'text-7',
+        'text-8',
+        'text-9'
+    ]
 ];
