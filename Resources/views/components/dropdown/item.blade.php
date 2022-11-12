bd65f22c1a3ee4d04743eb7b2c183aa0b163211d
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-12 00:24:01 -0300

    dropdown divider

--- Resources/views/components/dropdown/item.blade.php
+++ Resources/views/components/dropdown/item.blade.php
@@ -1,13 +1,23 @@
 @props([
-    'disabled' => false
+    'disabled' => false,
+    'header' => false,
 ])
-<li class="font-normal whitespace-nowrap bg-transparent w-full">
+<li @class([
+        "font-normal whitespace-nowrap w-full",
+        "bg-transparent" => !$header,
+        "bg-gray-200" => $header,
+        ])>
     <a @class([
         "dropdown-item text-sm py-2 px-4 block",
-        "text-gray-700 hover:bg-gray-100" => $disabled,
-        "text-gray-400 pointer-events-none" => $disabled
+        "text-gray-700" => $disabled,
+        "hover:bg-gray-100" => (!$header && !$disabled),
+        "text-gray-400" => $disabled,
+        "pointer-events-none" => ($disabled || $header),
+        "text-gray-500 font-semibold" => $header,
+        "{$attributes->get('class')}"
         ])
-       href="#" {{$attributes}}>
+       href="#"
+    >
         {{$slot}}
     </a>
 </li>
