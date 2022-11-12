f4398900fb63ba2f4a7307d0b20db8be6d9696f1
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-12 00:30:20 -0300

    dropdown item text

--- Resources/views/components/dropdown/item.blade.php
+++ Resources/views/components/dropdown/item.blade.php
@@ -1,6 +1,7 @@
 @props([
     'disabled' => false,
     'header' => false,
+    "no_action" => false,
 ])
 <li @class([
         "font-normal whitespace-nowrap w-full",
@@ -10,9 +11,9 @@
     <a @class([
         "dropdown-item text-sm py-2 px-4 block",
         "text-gray-700" => $disabled,
-        "hover:bg-gray-100" => (!$header && !$disabled),
+        "hover:bg-gray-100" => (!$header && !$disabled && !$no_action),
         "text-gray-400" => $disabled,
-        "pointer-events-none" => ($disabled || $header),
+        "pointer-events-none" => ($disabled || $header || $no_action),
         "text-gray-500 font-semibold" => $header,
         "{$attributes->get('class')}"
         ])
