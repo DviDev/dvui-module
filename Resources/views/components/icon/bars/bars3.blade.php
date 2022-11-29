8f0a4fdff291122e79839db1ed7fccd31c133df8
Davi Menezes <davimenezes.dev@gmail.com>
2022-11-28 22:02:51 -0300

    icons

--- Resources/views/components/icon/bars/bars3.blade.php
+++ Resources/views/components/icon/bars/bars3.blade.php
@@ -2,10 +2,10 @@
     'fill' => false,
     'mini' => false,
 ])
-<x-dvui::icon {{$attributes}} :outline="!$fill" :solid="$fill">
+<x-dvui::icon {{$attributes}} :outline="!$fill" :solid="$fill" stroke-width="2">
     @if($fill)
     @elseif($mini)
     @else
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
     @endif
 </x-dvui::icon>
