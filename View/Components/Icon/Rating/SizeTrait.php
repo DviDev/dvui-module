03211c8c43845e1ee241d68de8a5e9c680dbe424
Davi Menezes <davimenezes.dev@gmail.com>
2022-12-01 18:24:18 -0300

    social icons

--- View/Components/Icon/Rating/SizeTrait.php
+++ View/Components/Icon/Rating/SizeTrait.php
@@ -15,19 +15,5 @@ namespace Modules\DvUi\View\Components\Icon\Rating;
  */
 trait SizeTrait
 {
-//    public function __construct(
-//        public ?bool $s1 = null,
-//        public ?bool $s2 = null,
-//        public ?bool $s3 = null,
-//        public ?bool $s4 = null,
-//        public ?bool $s5 = null,
-//        public ?bool $s6 = null,
-//        public ?bool $s7 = null,
-//        public ?bool $s8 = null,
-//        public ?bool $s9 = null,
-//        public ?bool $s10 = null,
-//    )
-//    {
-//        //
-//    }
+
 }
