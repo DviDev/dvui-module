295a8598fa1d23740c547a9a65b8c489ef8e4760
Davi Menezes <davimenezes.dev@gmail.com>
2022-12-02 14:59:19 -0300

    spinners

---
+++ Resources/views/components/spinner.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'grow' => false,
+])
+@php
+    $classes = str($attributes->get('class'));
+    $occurrence = fn($str) => $classes->explode(' ')->filter(fn($i) => str($i)->startsWith($str) || str($i)->substrCount(" $str"));
+@endphp
+<div
+    {{$attributes->class([
+        "inline-block",
+        "spinner-border animate-spin" => !$grow,
+        "spinner-grow bg-current opacity-0" => $grow,
+        "w-8" => $occurrence('w-')->count() == 0,
+        "h-8" => $occurrence('h-')->count() == 0,
+        "border-4" => !$grow && $classes->substrCount('border-') == 0,
+        "rounded-full"
+    ])}}
+    role="status">
+    <span class="visually-hidden">__('loading')...</span>
+</div>
