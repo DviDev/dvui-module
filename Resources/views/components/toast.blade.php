3c5f5c65e389b3ad0cdd3b6c7f7d9b163ba464e2
Davi Menezes <davimenezes.dev@gmail.com>
2022-12-04 05:44:50 -0300

    toast

--- Resources/views/components/toast.blade.php
+++ Resources/views/components/toast.blade.php
@@ -8,24 +8,36 @@
     x-show="show"
      @class([
                     "absolute top-0 right-0 mr-4 mt-4
-                    bg-white shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block"])
+                     shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block"])
+     x-bind:class="toastType === 'primary' ? 'bg-blue-500' : 'bg-white'"
      id="static-example" role="alert" aria-live="assertive" aria-atomic="true" data-mdb-autohide="false">
     <div>
         <div class="border-2 bg-yellow-300 border-yellow-300 rounded-t"
              :style="{ width:width +'%' }"></div>
     </div>
-    <div class="bg-white flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-gray-200 rounded-t-lg">
+    <div class=" flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-gray-200 rounded-t-lg"
+         x-bind:class="toastType === 'primary' ? 'bg-blue-500' : 'bg-white'"
+
+    >
         <p class="font-bold flex space-x-1" x-bind:class="toastType === 'primary' ? 'text-white items-center' : 'text-gray-500'"
         >
             <x-dvui::icon.information.circle/>
             <span x-text="toastTitle">{{$title}}</span>
         </p>
-        <div class="flex items-center">
-            <p class="text-gray-600 text-xs" x-text="toastTitle2">{{$title2}}</p>
-            <button type="button" @click="closeToast()" class="btn-close box-content w-4 h-4 ml-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-mdb-dismiss="toast" aria-label="Close"></button>
+        <div class="flex items-center"
+        >
+            <p class="text-gray-600 text-xs" x-text="toastTitle2"
+               x-bind:class="toastType === 'primary' ? 'text-white' : ''"
+
+            >{{$title2}}</p>
+            <x-dvui::button @click="closeToast()">
+                <x-dvui::icon.x.circle s6/>
+            </x-dvui::button>
         </div>
     </div>
-    <div class="p-3 bg-white rounded-b-lg break-words text-gray-700" x-text="toastLabel">
+    <div class="p-3 rounded-b-lg break-words " x-text="toastLabel"
+         x-bind:class="toastType === 'primary' ? 'bg-blue-500 text-white' : 'bg-white text-gray-700'"
+    >
         {{$label}}
     </div>
 </div>
