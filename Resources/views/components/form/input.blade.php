a4b0846fbdfa452199f0ad0d92c5d19decd2aebc
Davi Menezes <davimenezes.dev@gmail.com>
2023-06-12 03:39:00 -0300

    query builder improvements

--- Resources/views/components/form/input.blade.php
+++ Resources/views/components/form/input.blade.php
@@ -23,7 +23,7 @@ if ($attr) {
             "peer block min-h-[auto] w-full rounded dark:border bg-transparent  px-3 outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0",
             "border-0 dark:border-gray-700" => !$errors->get('model.'.$attributes->get('id')),
             "border dark:border-red-500" => $errors->get('model.'.$attributes->get('id')),
-            'py-[0.32rem] leading-[1.6]' => false,
+            'py-[0.30rem] leading-[1.5]' => true,
         ])
         {{$attributes}}/>
     @error("model.{$attributes->get('id')}")
