4e1eea019c044602468554f50b5e4375365d5c13
Davi Menezes <davimenezes.dev@gmail.com>
2024-08-08 06:25:47 -0300

    trocando lista de proposta

--- View/BaseBladeComponent.php
+++ View/BaseBladeComponent.php
@@ -3,9 +3,12 @@
 namespace Modules\DvUi\View;
 
 use Illuminate\View\Component;
+use Modules\DvUi\Entities\ComponentCheck;
 
 abstract class BaseBladeComponent extends Component
 {
+    use ComponentCheck;
+
     public function __construct(
         public ?array          $attr = null,
         public ?string         $label = null,
@@ -13,6 +16,8 @@ abstract class BaseBladeComponent extends Component
         public ?bool            $validate = null,
         public                 $value = null,
         public string|int|null $id = null,
+        public ?bool   $disabled = null,
+        public ?string $placeholder = null,
     )
     {
         $this->attr['label'] = $label ?? $this->attr['label'] ?? null;
@@ -20,6 +25,8 @@ abstract class BaseBladeComponent extends Component
         $this->attr['validate'] = $validate ?? $this->attr['validate'] ?? false;
         $this->attr['value'] = $value ?? $this->attr['value'] ?? null;
         $this->attr['id'] = $id ?? $this->attr['id'] ?? null;
+        $this->attr['disabled'] = $this->disabled ?? $this->attr['disabled'] ?? null;
+        $this->attr['placeholder'] = $this->placeholder ?? $this->attr['placeholder'] ?? null;
     }
 
     public function getFormField($attributes)
