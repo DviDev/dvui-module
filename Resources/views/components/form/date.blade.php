61089f846e3f5fa555be0f723d732e6731eb3e05
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-22 00:10:45 -0300

    date components

---
+++ Resources/views/components/form/date.blade.php
@@ -0,0 +1,13 @@
+@php
+    use Modules\Base\View\Components\Form\BaseFormBladeComponent;
+
+    BaseFormBladeComponent::prepare($attributes);
+@endphp
+<x-dvui::form.field :validate="$attributes->get('validate')" :required="$attributes->get('required')" {{$attributes}}>
+    <div @class(["w-full flex flex-col"])>
+        @if($attributes->has('label'))
+            <label>{{$attributes->get('label')}}</label>
+        @endif
+        <input {{$attributes}}>
+    </div>
+</x-dvui::form.field>
