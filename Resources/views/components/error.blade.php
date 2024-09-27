48634cfbcdc74b05dcfbbc9a82fa703b9877e475
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-26 23:26:35 -0300

    refactorings

--- Resources/views/components/error.blade.php
+++ Resources/views/components/error.blade.php
@@ -1,3 +1,9 @@
+@php
+    $field = collect($attributes)->first(function($value, $key) {
+        return str($key)->contains('wire:model')
+            ?? $attributes['id'] ?? $attributes['name'] ?? $attributes->get('label');
+    })
+@endphp
 @error($field)
 <div class="text-red-600 text-sm leading-5" style="line-height: 17px">{{$message}}</div>
 @enderror
