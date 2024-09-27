65473dae2c5844a7d54c2139dd9dd6b7f772c2bb
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-26 23:26:43 -0300

    refactorings

--- View/Components/Error.php
+++ View/Components/Error.php
@@ -11,7 +11,7 @@ class Error extends Component
      *
      * @return void
      */
-    public function __construct(public string $field)
+    public function __construct()
     {
         //
     }
