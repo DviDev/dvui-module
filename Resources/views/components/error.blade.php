28fff2c7fdc4f488af2ea8ade60ded85f4117e1d
Davi Menezes <davimenezes.dev@gmail.com>
2024-10-12 23:57:51 -0300

    error component improvement

--- Resources/views/components/error.blade.php
+++ Resources/views/components/error.blade.php
@@ -1,9 +1,6 @@
-@php
-    $field = collect($attributes)->first(function($value, $key) {
-        return str($key)->contains('wire:model')
-            ?? $attributes['id'] ?? $attributes['name'] ?? $attributes->get('label');
-    })
-@endphp
+@props([
+    'field'
+])
 @error($field)
 <div class="text-red-600 text-sm leading-5" style="line-height: 17px">{{$message}}</div>
 @enderror
