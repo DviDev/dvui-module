16c7fb6569462ce6c8325e5bea44af1a2e710461
Davi Menezes <davimenezes.dev@gmail.com>
2025-07-10 12:20:46 -0300

    scope improvements

--- app/Contracts/MultiSelectSearchInterface.php
+++ app/Contracts/MultiSelectSearchInterface.php
@@ -5,5 +5,5 @@ namespace Modules\DvUi\Contracts;
 use Illuminate\Database\Eloquent\Builder;
 
 interface MultiSelectSearchInterface {
-    public static function apply(Builder $builder, array $searchFields, string $searchTerm, string $field): Builder;
+    public static function apply(Builder $builder, array $searchFields, string $searchTerm): Builder;
 }
