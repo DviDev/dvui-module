599e8cf67456f8f0717244f30292bcc222253c90
Davi Menezes <davimenezes.dev@gmail.com>
2025-07-19 05:06:43 -0300

    wip

---
+++ app/Interfaces/DvuiComponentSuiteContract.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\DvUi\Interfaces;
+
+interface DvuiComponentSuiteContract {
+    public function getSuiteIdentifier(): string;
+
+    public function getComponentMappings(): array;
+}
