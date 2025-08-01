c687395ce642b77b39a6d9b6b090035879b96568
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-01 17:09:00 -0300

    create config asset logo

--- Database/Seeders/DvUiDatabaseSeeder.php
+++ Database/Seeders/DvUiDatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Modules\DvUi\Database\Seeders;
 
 use Illuminate\Database\Eloquent\Model;
 use Modules\Base\Database\Seeders\BaseSeeder;
+use Modules\Base\Entities\Config\ConfigEntityModel;
+use Modules\Base\Models\ConfigModel;
+use Modules\Person\Entities\User\UserType;
+use Modules\Person\Models\UserTypeModel;
 
 class DvUiDatabaseSeeder extends BaseSeeder
 {
@@ -16,6 +20,17 @@ class DvUiDatabaseSeeder extends BaseSeeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $superAdminTypeModel = UserTypeModel::query()->where('name', UserType::SUPER_ADMIN->value)->first();
+
+        $superAdmin = BaseSeeder::firstOrCreateUser($superAdminTypeModel);
+
+        $config = ConfigEntityModel::props();
+        ConfigModel::query()->firstOrCreate([$config->name => 'app_logo'], [
+            $config->name => 'app_logo',
+            $config->value => asset('assets/img/dvi_logo.png'),
+            $config->description => 'application_logo',
+            $config->user_id => $superAdmin->id,
+            $config->default => 1,
+        ]);
     }
 }
