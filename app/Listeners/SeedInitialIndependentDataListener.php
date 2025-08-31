924661e492904fc3607a6c1badead5bdd86468be
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-30 23:42:17 -0300

    Update `SeedInitialIndependentDataListener` to handle `BaseSeederInitialIndependentDataEvent`
    Revised the event type in the listener to align with the new `BaseSeederInitialIndependentDataEvent` for improved consistency and functionality.

--- app/Listeners/SeedInitialIndependentDataListener.php
+++ app/Listeners/SeedInitialIndependentDataListener.php
@@ -4,12 +4,12 @@ namespace Modules\DvUi\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Artisan;
-use Modules\Base\Events\DatabaseSeederEvent;
+use Modules\Base\Events\BaseSeederInitialIndependentDataEvent;
 use Modules\DvUi\Database\Seeders\DvUiDatabaseSeeder;
 
 class SeedInitialIndependentDataListener implements ShouldQueue
 {
-    public function handle(DatabaseSeederEvent $event): void
+    public function handle(BaseSeederInitialIndependentDataEvent $event): void
     {
         Artisan::call('db:seed', ['--class' => DvUiDatabaseSeeder::class]);
     }
