31ef8e07498e445a84468a7f0f5040d357e49be7
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-01 17:59:52 -0300

    Rename `SeedInitialIndependentDataListener` to `SeedInitialIndependentDataDvUiListener` for clearer module-specific identification and update associated event bindings accordingly.

--- app/Providers/DvUiServiceProvider.php
+++ app/Providers/DvUiServiceProvider.php
@@ -9,7 +9,7 @@ use Modules\Base\Events\BaseSeederInitialIndependentDataEvent;
 use Modules\DvUi\Enums\DvuiComponentAlias;
 use Modules\DvUi\Interfaces\DvuiComponentSuiteContract;
 use Modules\DvUi\Listeners\CreateMenuItemsListener;
-use Modules\DvUi\Listeners\SeedInitialIndependentDataListener;
+use Modules\DvUi\Listeners\SeedInitialIndependentDataDvUiListener;
 use Modules\DvUi\Livewire\MultiSelectSearch;
 use Modules\DvUi\View\Components;
 use Modules\DvUi\View\Components\Alert;
@@ -72,7 +72,7 @@ class DvUiServiceProvider extends ServiceProvider
     {
         $this->app->register(RouteServiceProvider::class);
         \Event::listen(CreateMenuItemsEvent::class, CreateMenuItemsListener::class);
-        \Event::listen(BaseSeederInitialIndependentDataEvent::class, SeedInitialIndependentDataListener::class);
+        \Event::listen(BaseSeederInitialIndependentDataEvent::class, SeedInitialIndependentDataDvUiListener::class);
     }
 
     /**
