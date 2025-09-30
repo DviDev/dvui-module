68918410514aec591ccc5085118e0a0809b69c58
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-30 19:26:10 -0300

    pint

--- app/View/Components/Form/Select.php
+++ app/View/Components/Form/Select.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Modules\DvUi\View\Components\Form;
 
 use Illuminate\View\View;
-use Modules\DvUi\Enums\DvuiComponentAlias;
 use Modules\DvUi\Contracts\BaseBladeComponent;
+use Modules\DvUi\Enums\DvuiComponentAlias;
 
 final class Select extends BaseBladeComponent
 {
